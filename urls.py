c05e2dd68b41df087fdb2babaddffaf54ce0b386
Bernardo Silva <bfalmeidasilva@tecnico.ulisboa.pt>
2023-10-18 20:48:10 +0100

    Added smooth, oxigenio, sbsr, cidade, megahits

--- urls.py
+++ urls.py
@@ -3,11 +3,17 @@
 URL_ANTENA1 = "https://www.rtp.pt/play/livechannelonairnow.php?channel=at1"
 URL_ANTENA2 = " https://www.rtp.pt/play/livechannelonair.php?channel=92&howmanynext=0&howmanybefore=0&channeltype=radio"
 URL_ANTENA3 = "https://www.rtp.pt/play/livechannelonairnow.php?channel=at3"
+
+URL_CIDADEFM = "https://cidade.fm/nowplaying.xml"
 URL_COMERCIAL = "https://radiocomercial.pt/nowplaying.xml"
-URL_FUTURA = "https://public.radio.co/api/v2/s7e7b6c165/track/current"
 URL_M80 = "https://m80.pt/nowplaying.xml"
+URL_SMOOTH = "https://smoothfm.pt/nowplaying.xml" 
+
+URL_FUTURA = "https://public.radio.co/api/v2/s7e7b6c165/track/current"
 URL_RADAR = "https://radarlisboa.fm/avaplayer/onair.php"
-URL_MEGAHITS = "https://configsa01.blob.core.windows.net/megahits/megaOnAir.xml"
-URL_RFM = "https://configsa01.blob.core.windows.net/rfm/rfmOnAir.xml"
-URL_RENASCENCA = "https://configsa01.blob.core.windows.net/renascenca/rrOnAir.xml"
 URL_SBSR = "https://player.sbsr.fm/monitor/newMonitor.php"
+URL_OXIGENIO = "https://www.oxigenio.fm/avaplayer/onair.php"
+
+URL_MEGAHITS = "https://configsa01.blob.core.windows.net/megahits/megaOnAir.xml"
+URL_RENASCENCA = "https://configsa01.blob.core.windows.net/renascenca/rrOnAir.xml"
+URL_RFM = "https://configsa01.blob.core.windows.net/rfm/rfmOnAir.xml"
