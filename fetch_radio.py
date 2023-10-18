7a7a2fb01515a16df68afdbf9d53d32a9de1bfa6
Bernardo Silva <bfalmeidasilva@tecnico.ulisboa.pt>
2023-10-18 19:49:35 +0100

    Added antena 1 and antena 3

--- fetch_radio.py
+++ fetch_radio.py
@@ -6,9 +6,11 @@ import requests
 from bs4 import BeautifulSoup
 import xmltodict
 from urls import (
-    URL_RADAR,
+    URL_ANTENA1,
+    URL_ANTENA3,
     URL_COMERCIAL,
     URL_M80,
+    URL_RADAR,
 )
 
 @dataclass
@@ -55,7 +57,30 @@ def fetch_m80() -> Optional[Song]:
 
     return Song(title=title, artist=artist)
 
+def _fetch_antenax(url: str) -> Optional[Song]:
+    """ Fetches currently playing song and artist from Antena X """
+    result = requests.get(url).json()
+    try:
+        playing = result[0]
+        title = playing["dtitulo"]
+        artist = playing["dcoment1"]
+
+    except AttributeError:
+        return None
+
+    return Song(title=title, artist=artist)
+
+def fetch_antena1() -> Optional[Song]:
+    """ Fetches currently playing song and artist from Antena 1 """
+    return _fetch_antenax(URL_ANTENA1)
+
+def fetch_antena3() -> Optional[Song]:
+    """ Fetches currently playing song and artist from Antena 3 """
+    return _fetch_antenax(URL_ANTENA3)
+
 if __name__ == "__main__":
     print("Radar: ", fetch_radar())
     print("Comercial: ", fetch_comercial())
     print("M80: ", fetch_m80())
+    print("Antena1: ", fetch_antena1())
+    print("Antena3: ", fetch_antena3())
