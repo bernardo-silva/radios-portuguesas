355bdbcfd8c0dde6e2238df588fbb42186726365
Bernardo Silva <bfalmeidasilva@tecnico.ulisboa.pt>
2023-10-25 10:32:51 +0100

    Updated radio class

--- radio.py
+++ radio.py
@@ -25,25 +25,52 @@ class Radio:
 
     name: str
     url: str
-    img: str
+    image: str
     fetch_function: Callable
     current_song: Optional[SpotifySong] = None
     last_update: Optional[datetime] = None
 
+    def fetch(self):
+        song = self.fetch_function()
+        if song is not None:
+            self.last_update = datetime.now()
+            self.current_song = SpotifySong.from_search(song.title, song.artist)
 
-def available_radios() -> dict[str, Radio]:
-    return {
-        "antena1": Radio("Antena1", "", "", fetch_antena1),
-        "antena3": Radio("Antena3", "", "", fetch_antena3),
-        "comercial": Radio("Comercial", "", "", fetch_comercial),
-        "m80": Radio("M80", "", "", fetch_m80),
-        "cidadefm": Radio("CidadeFM", "", "", fetch_cidadefm),
-        "smooth": Radio("Smooth", "", "", fetch_smooth),
-        "futura": Radio("Futura", "", "", fetch_futura),
-        "radar": Radio("Radar", "", "", fetch_radar),
-        "oxigenio": Radio("Oxigénio", "", "", fetch_oxigenio),
-        "sbsr": Radio("SBSR", "", "", fetch_sbsr),
-        "megahits": Radio("Megahits", "", "", fetch_megahits),
-        "renascenca": Radio("Renascença", "", "", fetch_renascenca),
-        "rfm": Radio("RFM", "", "", fetch_rfm),
-    }
+
+def available_radios() -> list[Radio]:
+    return [
+        Radio("Antena1", "", "", fetch_antena1),
+        Radio(
+            "Antena3",
+            "https://www.rtp.pt/play/popup/antena3",
+            "images/antena3.webp",
+            fetch_antena3,
+        ),
+        Radio("Comercial", "", "", fetch_comercial),
+        Radio("M80", "", "", fetch_m80),
+        Radio("CidadeFM", "", "", fetch_cidadefm),
+        Radio("Smooth", "", "", fetch_smooth),
+        Radio("Futura", "", "", fetch_futura),
+        Radio("Radar", "https://radarlisboa.fm", "images/radar.png", fetch_radar),
+        Radio("Oxigénio", "", "", fetch_oxigenio),
+        Radio("SBSR", "", "", fetch_sbsr),
+        Radio("Megahits", "", "", fetch_megahits),
+        Radio("Renascença", "", "", fetch_renascenca),
+        Radio("RFM", "", "", fetch_rfm),
+    ]
+
+    # return {
+    #     "antena1": Radio("Antena1", "", "", fetch_antena1),
+    #     "antena3": Radio("Antena3", "", "", fetch_antena3),
+    #     "comercial": Radio("Comercial", "", "", fetch_comercial),
+    #     "m80": Radio("M80", "", "", fetch_m80),
+    #     "cidadefm": Radio("CidadeFM", "", "", fetch_cidadefm),
+    #     "smooth": Radio("Smooth", "", "", fetch_smooth),
+    #     "futura": Radio("Futura", "", "", fetch_futura),
+    #     "radar": Radio("Radar", "", "", fetch_radar),
+    #     "oxigenio": Radio("Oxigénio", "", "", fetch_oxigenio),
+    #     "sbsr": Radio("SBSR", "", "", fetch_sbsr),
+    #     "megahits": Radio("Megahits", "", "", fetch_megahits),
+    #     "renascenca": Radio("Renascença", "", "", fetch_renascenca),
+    #     "rfm": Radio("RFM", "", "", fetch_rfm),
+    # }
